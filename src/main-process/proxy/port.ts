dc1807bffac4ecd8559b453769f7c550b56fadd4
Gilad Gur Andelman <gilad@loadmill.com>
2023-04-03 10:27:48 +0300

    port range 1234-1239

--- src/main-process/proxy/port.ts
+++ src/main-process/proxy/port.ts
@@ -1,11 +1,12 @@
-import getPort from 'get-port';
+import getPort, { portNumbers } from 'get-port';
 
 import { sendFromProxyToRenderer } from '../../inter-process-communication/proxy-to-render';
 import log from '../../log';
 import { GET_PORT, PORT } from '../../universal/constants';
 import { subscribeToMainProcessMessage } from '../main-events';
 
-const DESIRED_PORTS = [8080, 8081, 9090, 9091];
+const PORT_MIN_RANGE = 1234;
+const PORT_MAX_RANGE = 1239;
 
 let port: number;
 
@@ -14,7 +15,7 @@ let port: number;
  * @returns The port that was found to be available
  */
 export const initToAvailablePort = async (): Promise<number> => {
-  port = await getPort({ port: DESIRED_PORTS });
+  port = await getPort({ port: portNumbers(PORT_MIN_RANGE, PORT_MAX_RANGE) });
   log.info(`Proxy port set to ${port}`);
   return port;
 };
