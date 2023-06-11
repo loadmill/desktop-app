33b87920b7cd129aa9fa97375732bdeca913bc8c
Gilad Gur Andelman <gilad@loadmill.com>
2023-06-11 13:43:27 +0300

    🛣  Trust Loadmill hostname

--- src/main-process/call-loadmill-api.ts
+++ src/main-process/call-loadmill-api.ts
@@ -1,12 +1,19 @@
+import https from 'https';
+
 import fetch, { RequestInit, Response } from 'node-fetch';
 
 import { LOADMILL_WEB_APP_ORIGIN } from './constants';
 import { getCookie } from './cookies';
 
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
 export const callLoadmillApi = async (path: string, init: RequestInit = {}): Promise<Response> => {
   const cookie = await getCookie();
   const { headers = {}, ...rest } = init;
   const response = await fetch(LOADMILL_WEB_APP_ORIGIN + '/' + path, {
+    agent: httpsAgent,
     headers: { ...headers, 'cookie': cookie },
     ...rest,
   });
