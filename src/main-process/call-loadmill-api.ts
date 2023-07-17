79d07ed0a0e5dc12650e5e4a955d0aef4e55772f
Gilad Gur Andelman <gilad@loadmill.com>
2023-07-17 13:26:16 +0300

    🛣  Allowing self signed certificates

--- src/main-process/call-loadmill-api.ts
+++ src/main-process/call-loadmill-api.ts
@@ -1,21 +1,16 @@
-import https from 'https';
-
-import fetch, { RequestInit, Response } from 'node-fetch';
+import log from '../log';
 
 import { LOADMILL_WEB_APP_ORIGIN } from './constants';
 import { getCookie } from './cookies';
-
-const httpsAgent = new https.Agent({
-  rejectUnauthorized: false,
-});
+import { fetch, RequestInit, Response } from './fetch';
 
 export const callLoadmillApi = async (path: string, init: RequestInit = {}): Promise<Response> => {
   const cookie = await getCookie();
   const { headers = {}, ...rest } = init;
   const response = await fetch(LOADMILL_WEB_APP_ORIGIN + '/' + path, {
-    agent: httpsAgent,
     headers: { ...headers, 'cookie': cookie },
     ...rest,
   });
+  log.info('Called Loadmill API', { path });
   return response;
 };
