21f158408db27d111aa1a32ab897253c7f35680c
Gilad Gur Andelman <gilad@loadmill.com>
2025-05-20 09:31:56 +0300

    bundle only the target platform + arch & no extraResources

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -3,14 +3,19 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
+const nodePathByPlatformAndArch = process.platform === 'win32' ?
+  path.join('windows', 'x64') :
+  path.join('macos', process.arch === 'arm64' ? 'arm64' : 'x64');
+const bundledNodePath = path.join('bundled_node', nodePathByPlatformAndArch);
+
 module.exports = [
   new ForkTsCheckerWebpackPlugin(),
   new CopyWebpackPlugin({
     patterns: [
       {
-        from: 'bundled_node',
+        from: bundledNodePath,
         info: { minimized: true },
-        to: path.resolve(__dirname, '.webpack/main/bundled_node'),
+        to: path.resolve(__dirname, path.join('.webpack', 'main', bundledNodePath)),
       },
     ],
   }),
