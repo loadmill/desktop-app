bc412605bcc17478528ed5e3bd2b31592ec06361
Gilad Gur Andelman <gilad@loadmill.com>
2025-06-22 11:28:12 +0300

    use @loadmill/agent@0.1.169

--- src/main-process/agent-handlers.ts
+++ src/main-process/agent-handlers.ts
@@ -79,22 +79,20 @@ const LOADMILL_AGENT = 'loadmill-agent';
 const LOADMILL_AGENT_PATH = path.join(PACKED_RELATIVE_PATH, LOADMILL_AGENT);
 
 const createAgentProcess = (): ChildProcessWithoutNullStreams => {
-  log.info('Creating agent process with env vars', {
+  const env = {
+    HOME_DIR: app.getPath('userData'),
     LOADMILL_AGENT_SERVER_URL,
+    LOADMILL_AGENT_VERBOSE,
+    NODE_OPTIONS,
     NODE_TLS_REJECT_UNAUTHORIZED,
-  });
+    PLAYWRIGHT_BROWSERS_PATH: '0',
+    PLAYWRIGHT_TEST_PACKAGE_CLI_PATH,
+    UI_TESTS_ENABLED,
+  };
+  log.info('Creating agent process with env vars', env);
 
   return fork(LOADMILL_AGENT_PATH, {
-    env: {
-      HOME_DIR: app.getPath('userData'),
-      LOADMILL_AGENT_SERVER_URL,
-      LOADMILL_AGENT_VERBOSE,
-      NODE_OPTIONS,
-      NODE_TLS_REJECT_UNAUTHORIZED,
-      PLAYWRIGHT_BROWSERS_PATH: '0',
-      PLAYWRIGHT_TEST_PACKAGE_CLI_PATH,
-      UI_TESTS_ENABLED,
-    },
+    env,
     stdio: 'pipe',
   });
 };
