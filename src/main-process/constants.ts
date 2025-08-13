328c881ecfa6cc9802257cc21992b861dbc76c5b
Gilad Gur Andelman <gilad@loadmill.com>
2025-08-13 13:37:16 +0300

    💡 Symlink the entire @playwright package instead of copying it & refactor User Data Path

--- src/main-process/constants.ts
+++ src/main-process/constants.ts
@@ -5,11 +5,12 @@ import { app } from 'electron';
 
 config();
 
+export const USER_DATA_PATH = app.getPath('userData');
 export const LOADMILL_AGENT_VERBOSE = process.env.LOADMILL_AGENT_VERBOSE;
 export const UI_TESTS_ENABLED = process.env.UI_TESTS_ENABLED || 'true';
 const STANDALONES_PATH = process.env.NODE_ENV === 'production' ? process.resourcesPath : app.getAppPath();
 export const STANDALONE_PLAYWRIGHT_DIR_PATH = path.join(STANDALONES_PATH, 'standalone_playwright');
-export const PLAYWRIGHT_TEST_PACKAGE_CLI_PATH = path.join(app.getPath('userData'), 'node_modules', '@playwright', 'test', 'cli.js');
+export const PLAYWRIGHT_TEST_PACKAGE_CLI_PATH = path.join(USER_DATA_PATH, 'node_modules', '@playwright', 'test', 'cli.js');
 export const NODE_TLS_REJECT_UNAUTHORIZED = process.env.NODE_TLS_REJECT_UNAUTHORIZED || '0';
 export const NODE_OPTIONS = process.env.NODE_OPTIONS || '--http-parser=legacy  --max-http-header-size=20000';
 export const CALLBACK_URL = process.env.CALLBACK_URL;
