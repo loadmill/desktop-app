c895f8604795c7d177a884a513457661c6a88afb
Gilad Gur Andelman <gilad@loadmill.com>
2025-12-16 12:50:30 +0200

    〽️ Symlink Check

--- src/main-process/standalone-playwright/symlink-playwright.ts
+++ src/main-process/standalone-playwright/symlink-playwright.ts
@@ -1,4 +1,3 @@
-
 import fs from 'fs';
 import path from 'path';
 
@@ -14,8 +13,15 @@ import {
 export const symlinkPlaywright = (): void => {
   log.info('Starting to set up Playwright package in user data path...');
   try {
-    _removePlaywrightPackageIfExists();
-    _createSymlink();
+    const symlinkNodeModulesPath = path.join(USER_DATA_PATH, 'node_modules');
+
+    if (_isSymlink(symlinkNodeModulesPath)) {
+      log.info('node_modules is already a symlink, nothing to do');
+    } else {
+      log.info('node_modules is not a symlink, proceeding to set up symlink...');
+      _removePlaywrightPackageIfExists();
+      _createSymlink();
+    }
   } catch (error) {
     log.error('Failed to set up Playwright package in user data path:', error);
     return;
@@ -35,22 +41,32 @@ const _removePlaywrightPackageIfExists = (): void => {
 
   for (const p of pathsToRemove) {
     if (fs.existsSync(p)) {
-      log.info(`Removing existing path: ${p}`);
+      log.info(`Removing old file: ${p}`);
       fs.rmSync(p, { force: true, recursive: true });
     }
   }
+  log.info('Old Playwright package removal process completed.');
 };
 
 const _createSymlink = (): void => {
-  const userDataPath = USER_DATA_PATH;
+  const dest = path.join(USER_DATA_PATH, 'node_modules');
   const src = path.join(STANDALONE_PLAYWRIGHT_DIR_PATH, 'node_modules');
-  const dest = path.join(userDataPath, 'node_modules');
 
+  log.info('Creating symlink for Playwright package...', { dest, src });
   if (process.platform === 'win32') {
     log.info('Running on Windows, using junction for symlink...');
     fs.symlinkSync(src, dest, 'junction');
   } else {
+    log.info('Running on non-Windows OS, using dir for symlink...');
     fs.symlinkSync(src, dest, 'dir');
   }
   log.info('Symlink created successfully', { dest, src });
+};
+
+const _isSymlink = (p: string): boolean => {
+  try {
+    return fs.lstatSync(p).isSymbolicLink();
+  } catch {
+    return false;
+  }
 };
